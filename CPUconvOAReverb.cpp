090488f26c2ef376277c3f1f055993808c3175b5
Yannick Bäumer <yannick.baeumer@student.hpi.de>
2018-02-07 18:00:29 +0100

    added merge function for overlap add

--- CPUconvOAReverb.cpp
+++ CPUconvOAReverb.cpp
@@ -26,17 +26,18 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 	fftw_complex* targetSignalsLFT = new fftw_complex[resultSignalSize];
 	fftw_complex* targetSignalsRFT = new fftw_complex[resultSignalSize];
 
+	fftw_complex* mergedSignalL = new fftw_complex[targetFrames + impulseFrames - 1];
+	fftw_complex* mergedSignalR = new fftw_complex[targetFrames + impulseFrames - 1];
+
 	padTargetSignal(target, sampleCount, impulseFrames, paddedTargetSignal);
-	printComplexArray(paddedTargetSignal, impulseFrames);
 
 	// copy impulse sound to complex buffer
-	for (int i = 0; i < sampleSize; ++i)
-	{
-		if(i < impulseFrames){
+	for (int i = 0; i < sampleSize; ++i) {
+		if(i < impulseFrames) {
 			impulseSignalL[i][0] = impulseL[i];
 			impulseSignalR[i][0] = impulseR[i];
 		}
-		else{
+		else {
 			impulseSignalL[i][0] = 0.0f;
 			impulseSignalR[i][0] = 0.0f;
 		}
@@ -53,8 +54,7 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 
 	// fourrier transform of target and impulse signal
 	// resultSignalSize, targetSignal, targetSignalFt
-	for (int i = 0; i < sampleCount; i += sampleSize)
-	{
+	for (int i = 0; i < sampleCount; i += sampleSize) {
 		// colvolve only parts of the input and output buffers
 		convolve(&paddedTargetSignal[i], impulseSignalL, &targetSignalsLFT[i], sampleSize);
 		convolve(&paddedTargetSignal[i], impulseSignalR, &targetSignalsRFT[i], sampleSize);
@@ -66,17 +66,23 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 	transformedR_plan_backward = fftw_plan_dft_1d(resultSignalSize, targetSignalsRFT, targetSignalRIFT, FFTW_BACKWARD, FFTW_ESTIMATE);
 	fftw_execute(transformedR_plan_backward);
 
+	printComplexArray(targetSignalLIFT, resultSignalSize);
+
 	float maxo[2];
 	maxo[0]=0.0f;
 	maxo[1]=0.0f;  
 
-	for (int i = 0; i < resultSignalSize; i++){
-		if (abs(maxo[0])<=abs(targetSignalLIFT[i][0])) maxo[0]=targetSignalLIFT[i][0];
-		if (abs(maxo[1])<=abs(targetSignalRIFT[i][0])) maxo[1]=targetSignalRIFT[i][0];
+	for (int i = 0; i < resultSignalSize; i++) {
+		if (abs(maxo[0])<=abs(targetSignalLIFT[i][0])) {
+			maxo[0]=targetSignalLIFT[i][0];
+		}
+		if (abs(maxo[1])<=abs(targetSignalRIFT[i][0])) {
+			maxo[1]=targetSignalRIFT[i][0];
+		}
 	}
 	float maxot= abs(maxo[0])>=abs(maxo[1])? abs(maxo[0]): abs(maxo[1]);
 
-	for (int i=0; i< resultSignalSize; i++){
+	for (int i=0; i< resultSignalSize; i++) {
 		//printf("%f\n", targetSignalLIFT[i][0]);
 		float temp=0.0f;
 		outputL[i]= (float)((targetSignalLIFT[i][0])/(maxot));
@@ -110,7 +116,7 @@ uint32_t convolve(fftw_complex* targetSignal,
 	fftw_plan target_plan_forward = fftw_plan_dft_1d(sampleSize, targetSignal, transformedSignal, FFTW_FORWARD, FFTW_ESTIMATE);	
 	fftw_execute(target_plan_forward);
 
-	for (int i = 0; i < sampleSize; i++){
+	for (int i = 0; i < sampleSize; i++) {
 		transformedSignal[i][0]= ((impulseSignal[i][0]*transformedSignal[i][1])+(impulseSignal[i][1]*transformedSignal[i][0]));
 		transformedSignal[i][1]= ((impulseSignal[i][0]*transformedSignal[i][1])+(impulseSignal[i][1]*transformedSignal[i][0]));
 	}
@@ -123,19 +129,16 @@ uint32_t padTargetSignal(float* target, uint32_t sampleCount, uint32_t sampleSiz
 	// cut the target signal into samplecount buffers
 	uint32_t stride = sampleSize * 2 - 1;
 
-	for (int i = 0; i < sampleCount; ++i)
-	{
+	for (int i = 0; i < sampleCount; ++i) {
 		// copy targetsignal into new buffer
-		for (int k = 0; k < sampleSize; ++k)
-		{
+		for (int k = 0; k < sampleSize; ++k) {
 			int offset = i * stride + k;
 			destinationBuffer[offset][0] = target[offset];
 			destinationBuffer[offset][1] = 0.0f;
 		}
 
 		// pad the buffer with zeros til it reaches a size of samplecount * 2 - 1
-		for (int k = 0; k < sampleSize - 1; ++k)
-		{
+		for (int k = 0; k < sampleSize - 1; ++k) {
 			int offset = i * stride + sampleSize + k;
 			destinationBuffer[offset][0] = 0.0f;
 			destinationBuffer[offset][1] = 0.0f;
@@ -143,6 +146,29 @@ uint32_t padTargetSignal(float* target, uint32_t sampleCount, uint32_t sampleSiz
 	}
 
 	return sampleCount * 2 - 1;
+}
+
+uint32_t mergeConvolutedSignal(fftw_complex *longInputBuffer, fftw_complex *shortOutpuBuffer, uint32_t sampleSize, uint32_t sampleCount) {
+	
+	// copy first sample as it is into output buffer
+	for (int i = 0; i < sampleSize; ++i) {
+		shortOutpuBuffer[i][0] = longInputBuffer[i][0];
+		shortOutpuBuffer[i][1] = longInputBuffer[i][1];
+	}
+
+	uint stride = sampleCount * 2 - 1;
+	// start with second sample, the first one has no signal tail to merge with
+	for (int i = 1; i < sampleCount; ++i) {
+
+		// signal tail length is sampleSize - 1, so the last element has nothing to be added to
+		for (int k = 0; i < sampleSize - 1; ++k) {
+			shortOutpuBuffer[stride + k][0] = longInputBuffer[stride + k][0] + longInputBuffer[stride - sampleSize + k][0];
+			shortOutpuBuffer[stride + k][1] = longInputBuffer[stride + k][1] + longInputBuffer[stride - sampleSize + k][1];
+		}
+		// set last element to the same as in input buffer
+		shortOutpuBuffer[stride + sampleSize][0] = shortOutpuBuffer[stride + sampleSize][0];
+		shortOutpuBuffer[stride + sampleSize][1] = shortOutpuBuffer[stride + sampleSize][1];
+	}
 }
 
 void printComplexArray(fftw_complex *target, uint32_t size) {
