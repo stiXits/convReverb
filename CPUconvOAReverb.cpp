bb848d88f05087b14ba0205109ba8e65cc59d663
Yannick Bäumer <yannick.baeumer@student.hpi.de>
2018-02-08 16:14:00 +0100

    integrated merge and fixed some errors

--- CPUconvOAReverb.cpp
+++ CPUconvOAReverb.cpp
@@ -16,15 +16,10 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 	fftw_complex* impulseSignalR = new fftw_complex[sampleSize];
 	fftw_complex* impulseSignalRFT = new fftw_complex[sampleSize];
 
-	fftw_complex* targetSignalLIFT = new fftw_complex[resultSignalSize];
-	fftw_complex* targetSignalRIFT = new fftw_complex[resultSignalSize];
-
-	fftw_complex* transformedSignalL = new fftw_complex[resultSignalSize];
-	fftw_complex* transformedSignalR = new fftw_complex[resultSignalSize];
-
 	fftw_complex* paddedTargetSignal = new fftw_complex[resultSignalSize];
-	fftw_complex* targetSignalsLFT = new fftw_complex[resultSignalSize];
-	fftw_complex* targetSignalsRFT = new fftw_complex[resultSignalSize];
+
+	fftw_complex* convolvedSignalL = new fftw_complex[resultSignalSize];
+	fftw_complex* convolvedSignalR = new fftw_complex[resultSignalSize];
 
 	fftw_complex* mergedSignalL = new fftw_complex[targetFrames + impulseFrames - 1];
 	fftw_complex* mergedSignalR = new fftw_complex[targetFrames + impulseFrames - 1];
@@ -56,37 +51,32 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 	// resultSignalSize, targetSignal, targetSignalFt
 	for (int i = 0; i < sampleCount; i += sampleSize) {
 		// colvolve only parts of the input and output buffers
-		convolve(&paddedTargetSignal[i], impulseSignalL, &targetSignalsLFT[i], sampleSize);
-		convolve(&paddedTargetSignal[i], impulseSignalR, &targetSignalsRFT[i], sampleSize);
+		convolve(&paddedTargetSignal[i], impulseSignalL, &convolvedSignalL[i], impulseFrames);
+		convolve(&paddedTargetSignal[i], impulseSignalR, &convolvedSignalR[i], impulseFrames);
 	}
 
 	// backward fourrier transform on transformed signal
-	transformedL_plan_backward = fftw_plan_dft_1d(resultSignalSize, targetSignalsLFT, targetSignalLIFT, FFTW_BACKWARD, FFTW_ESTIMATE);
-	fftw_execute(transformedL_plan_backward);
-	transformedR_plan_backward = fftw_plan_dft_1d(resultSignalSize, targetSignalsRFT, targetSignalRIFT, FFTW_BACKWARD, FFTW_ESTIMATE);
-	fftw_execute(transformedR_plan_backward);
+	// transformedL_plan_backward = fftw_plan_dft_1d(resultSignalSize, targetSignalsLFT, targetSignalLIFT, FFTW_BACKWARD, FFTW_ESTIMATE);
+	// fftw_execute(transformedL_plan_backward);
+	// transformedR_plan_backward = fftw_plan_dft_1d(resultSignalSize, targetSignalsRFT, targetSignalRIFT, FFTW_BACKWARD, FFTW_ESTIMATE);
+	// fftw_execute(transformedR_plan_backward);
 
-	printComplexArray(targetSignalLIFT, resultSignalSize);
+	//printComplexArray(convolvedSignalL, resultSignalSize);
 
 	float maxo[2];
 	maxo[0]=0.0f;
-	maxo[1]=0.0f;  
+	maxo[1]=0.0f; 
 
-	for (int i = 0; i < resultSignalSize; i++) {
-		if (abs(maxo[0])<=abs(targetSignalLIFT[i][0])) {
-			maxo[0]=targetSignalLIFT[i][0];
-		}
-		if (abs(maxo[1])<=abs(targetSignalRIFT[i][0])) {
-			maxo[1]=targetSignalRIFT[i][0];
-		}
-	}
+	maxo[0] = maximum(maxo[0], mergeConvolvedSignal(convolvedSignalL, mergedSignalL, sampleSize, sampleCount)); 
+	maxo[1] = maximum(maxo[1], mergeConvolvedSignal(convolvedSignalR, mergedSignalR, sampleSize, sampleCount)); 
+
 	float maxot= abs(maxo[0])>=abs(maxo[1])? abs(maxo[0]): abs(maxo[1]);
 
-	for (int i=0; i< resultSignalSize; i++) {
+	for (int i=0; i< targetFrames + impulseFrames - 1; i++) {
 		//printf("%f\n", targetSignalLIFT[i][0]);
 		float temp=0.0f;
-		outputL[i]= (float)((targetSignalLIFT[i][0])/(maxot));
-		outputR[i]= (float)((targetSignalRIFT[i][0])/(maxot));
+		outputL[i]= (float)((mergedSignalL[i][0])/(maxot));
+		outputR[i]= (float)((mergedSignalR[i][0])/(maxot));
 	}
 
 	// delete [] paddedTargetSignal;
@@ -102,8 +92,8 @@ uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulseL, 
 	// delete [] targetSignalLIFT;
 	// delete [] targetSignalRIFT;
 
-	// delete [] transformedSignalL;
-	// delete [] transformedSignalR;
+	// delete [] convolvedSignalL;
+	// delete [] convolvedSignalR;
 
 	return resultSignalSize;
 }
@@ -113,13 +103,22 @@ uint32_t convolve(fftw_complex* targetSignal,
 	fftw_complex* transformedSignal,
 	uint32_t sampleSize) {
 
+	// transform signal to frequency domaine
 	fftw_plan target_plan_forward = fftw_plan_dft_1d(sampleSize, targetSignal, transformedSignal, FFTW_FORWARD, FFTW_ESTIMATE);	
 	fftw_execute(target_plan_forward);
 
+	// convolve target and signal
 	for (int i = 0; i < sampleSize; i++) {
 		transformedSignal[i][0]= ((impulseSignal[i][0]*transformedSignal[i][1])+(impulseSignal[i][1]*transformedSignal[i][0]));
 		transformedSignal[i][1]= ((impulseSignal[i][0]*transformedSignal[i][1])+(impulseSignal[i][1]*transformedSignal[i][0]));
 	}
+
+	// transform result back to time domaine
+	fftw_plan target_plan_backward = fftw_plan_dft_1d(sampleSize, transformedSignal, targetSignal , FFTW_BACKWARD, FFTW_ESTIMATE);	
+	fftw_execute(target_plan_forward);
+
+	// switch buffers
+	transformedSignal = impulseSignal;
 
 	return sampleSize;
 }
@@ -148,27 +147,43 @@ uint32_t padTargetSignal(float* target, uint32_t sampleCount, uint32_t sampleSiz
 	return sampleCount * 2 - 1;
 }
 
-uint32_t mergeConvolutedSignal(fftw_complex *longInputBuffer, fftw_complex *shortOutpuBuffer, uint32_t sampleSize, uint32_t sampleCount) {
-	
+float mergeConvolvedSignal(fftw_complex *longInputBuffer, fftw_complex *shortOutpuBuffer, uint32_t sampleSize, uint32_t sampleCount) {
+	float max = 0; 
+
 	// copy first sample as it is into output buffer
 	for (int i = 0; i < sampleSize; ++i) {
+
 		shortOutpuBuffer[i][0] = longInputBuffer[i][0];
 		shortOutpuBuffer[i][1] = longInputBuffer[i][1];
+		max = maximum(max, longInputBuffer[i][0]);
 	}
 
-	uint stride = sampleCount * 2 - 1;
+	uint stride = sampleSize * 2 - 1;
 	// start with second sample, the first one has no signal tail to merge with
 	for (int i = 1; i < sampleCount; ++i) {
+		uint32_t step = stride * i;
 
 		// signal tail length is sampleSize - 1, so the last element has nothing to be added to
-		for (int k = 0; i < sampleSize - 1; ++k) {
-			shortOutpuBuffer[stride + k][0] = longInputBuffer[stride + k][0] + longInputBuffer[stride - sampleSize + k][0];
-			shortOutpuBuffer[stride + k][1] = longInputBuffer[stride + k][1] + longInputBuffer[stride - sampleSize + k][1];
+		for (int k = 0; k < sampleSize - 1; ++k) {
+			shortOutpuBuffer[step + k][0] = longInputBuffer[step + k][0] + longInputBuffer[step - sampleSize + k][0];
+			shortOutpuBuffer[step + k][1] = longInputBuffer[step + k][1] + longInputBuffer[step - sampleSize + k][1];
+			max = maximum(max, longInputBuffer[step + k][0]);
 		}
 		// set last element to the same as in input buffer
-		shortOutpuBuffer[stride + sampleSize][0] = shortOutpuBuffer[stride + sampleSize][0];
-		shortOutpuBuffer[stride + sampleSize][1] = shortOutpuBuffer[stride + sampleSize][1];
+		shortOutpuBuffer[step + sampleSize][0] = shortOutpuBuffer[step + sampleSize][0];
+		shortOutpuBuffer[step + sampleSize][1] = shortOutpuBuffer[step + sampleSize][1];
+		max = maximum(max, longInputBuffer[step + sampleSize][0]);
 	}
+
+	return max;
+}
+
+inline float maximum(float max, float value) {
+	if (abs(max) <= abs(value)) {
+		max = value;
+	}
+
+	return max;
 }
 
 void printComplexArray(fftw_complex *target, uint32_t size) {
--- CPUconvOAReverb.h
+++ CPUconvOAReverb.h
@@ -6,5 +6,6 @@
 uint32_t CPUconvOAReverb(float *target, uint32_t targetFrames, float *impulsesx, float *impulsedx, uint32_t impulseFrames, float *outputsx, float *outputdx);
 uint32_t padTargetSignal(float* target, uint32_t sampleCount, uint32_t sampleSize, fftw_complex* destinationBuffer);
 uint32_t convolve(fftw_complex* targetSignal, fftw_complex* impulseSignal, fftw_complex* transformedSignal,uint32_t sampleSize);
-uint32_t mergeConvolutedSignal(fftw_complex *longInputBuffer, fftw_complex *shortOutpuBuffer, uint32_t sampleSize, uint32_t sampleCount);
+float mergeConvolvedSignal(fftw_complex *longInputBuffer, fftw_complex *shortOutpuBuffer, uint32_t sampleSize, uint32_t sampleCount);
 void printComplexArray(fftw_complex *target, uint32_t size);
+inline float maximum(float maxo, float value);
